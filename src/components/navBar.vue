b33e5a6f42d350909e93a8cdfac5300c55b3a33f
DanielVasc <estudante.danielvasc@gmail.com>
2024-12-16 11:56:11 -0300

    feito

---
+++ src/components/navBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/filmes">Filmes</router-link>
+            <router-link to="/tv">Séries</router-link>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header {
+    background-color: #2c3e50; 
+    color: #ffffff; 
+    padding: 1rem;
+    text-align: center;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3); 
+}
+
+nav {
+    display: flex;
+    justify-content: center; 
+    column-gap: 2rem;
+}
+
+nav a {
+    text-decoration: none;
+    color: #ffffff; 
+    font-weight: bold; 
+    transition: color 0.3s; 
+}
+
+nav a:hover {
+    color: #f39c12; 
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Home</h1>
+    </div>
+
+</template>
+
+<style scoped>
+.header {
+    background-color: var(--vt-c-indigo);
+    color: var(--color-background);
+    padding: 1rem;
+    text-align: center;
+}
+
+.featured-section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 2rem 0;
+}
+
+.featured-title {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+}
+
+.featured-list {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    gap: 1rem;
+}
+
+.featured-item {
+    width: 20rem;
+    border-radius: 0.5rem;
+    overflow: hidden;
+    box-shadow: 0 0 0.5rem #000;
+    transition: transform 0.3s; 
+}
+
+.featured-item:hover {
+    transform: scale(1.05); 
+}
+
+.featured-item img {
+    width: 100%;
+    height: 15rem;
+    border-radius: 0.5rem;
+}
+
+.featured-description {
+    padding: 0.5rem;
+    text-align: center;
+}
+</style>
